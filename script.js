3f03711db72b7ac738167b1dba7f377e4dcf6f57
brahimmeliani29-sketch <brahimmeliani29@gmail.com>
2026-09-13 01:51:22 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function changeColor(){
+const letters='0123456789ABCDEF';
+let color='#';
+for(let i=0;i<6;i++){
+    color+=letters[Math.floor(Math.random()*16)];
+}
+document.body.style.backgroundColor=color;
+}    
